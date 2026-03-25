ba42ec717cf3ff4ee73eaa8fb84dff75283b6734
Antoine 🌿 <pro.antoinegib@gmail.com>
2026-03-25 16:49:15 +0100

    Update config.js pour render

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
   development: {
@@ -18,11 +18,13 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    use_env_variable: 'DATABASE_URL', // <--- important
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Render oblige SSL
+      },
+    },
   },
-}
+};
